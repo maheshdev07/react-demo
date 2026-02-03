d045fbbc7500667e87c6458ec31b62c6501a0479
G MAHESH <mahesh.dev07@kgrp.in>
2026-02-03 11:14:25 +0530

    added reset button

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,13 @@ function App() {
       <button onClick={() => setCount(count - 1)} style={{ marginLeft: "10px" }}>
         -
       </button>
+      <button
+       onClick={() => setCount(0)}
+      style={{ marginLeft: "10px" }}
+        >
+  Reset
+</button>
+
     </div>
   );
 }
